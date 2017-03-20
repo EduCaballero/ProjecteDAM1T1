a3cfc8486ebce06b9fcdf434113c02a7d67fb819
ByNeon <alexlopezbox@gmail.com>
2017-03-20 21:46:23 +0100

    nuevo borrado

--- public_htmlNUEVO/municipios.php
+++
@@ -1,8 +0,0 @@
-<?php
-	require_once 'bbdd_1.php';
-
-	$id = $_POST["id"];
-	$municipios = selectMunicipiosByProv($id);
-	$data = $municipios->fetch_all(MYSQLI_ASSOC);
-	echo (json_encode($data));
- ?>
